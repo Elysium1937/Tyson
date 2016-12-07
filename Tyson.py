83c9a744c0b8a56ddb9d865732c1a3fb05caf76e
Guy Sapire <guysapire@gmail.com>
2016-12-07 21:42:16 +0200

    Current version working on test1.jpg!

--- Tyson.py
+++ Tyson.py
@@ -2,19 +2,8 @@ import numpy
 import cv2
 import time
 
-max_bounding_rect_edge_ratio = 1.2
-
-def brightnessFiltering(img):
-    """
-    this function filters out the darker pixels
-    """
-
-    hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-    lowerLimit = numpy.array([20,0,50])
-    upperLimit = numpy.array([110,255,255])
-    mask = cv2.inRange(hsv, lowerLimit, upperLimit)
-
-    return mask
+MAX_BOUNDING_RECT_AREA_DIFFERENCE = 1.15
+AREA_THRESHOLD = 400
 
 def shapeFiltering(contourList):
     """
@@ -23,64 +12,83 @@ def shapeFiltering(contourList):
     """
 
     outputList = []
+    
     if type(contourList) is not list:
         contourList = [contourList]
 
     for currentContour in contourList:
         _, _, w, h = cv2.boundingRect(currentContour)
-        #currentBoundingRect = cv2.boundingRect(currentContour)
-        #w = currentBoundingRect[0][0] - currentBoundingRect[0][1]
-        #h = currentBoundingRect[0][0] - currentBoundingRect[1][0]
-        minEdge = min(h, w)
-        maxEdge = max(h, w)
-
-        if maxEdge / minEdge < max_bounding_rect_edge_ratio:
+        boundingRectArea = w*h 
+        currentContourArea = cv2.contourArea(currentContour)
+        if boundingRectArea / currentContourArea < MAX_BOUNDING_RECT_AREA_DIFFERENCE:
             outputList.append(currentContour)
 
     return outputList
 
-def sortContours(contour_list):
+def areaFiltering(contourList):
     """
-    this function sorts the list of retroreflectors from left to right
+    filters contours that are not above the threshold
     """
 
-    if type(contour_list) is list and len(contour_list) is not 0:
-        sorted_list = sorted(contour_list, key = lambda contour: cv2.boundingRect(contour)[0] + 0.5*cv2.boundingRect(contour)[2])
-    else:
-        sorted_list = []
+    outputList = []
+    
+    if type(contourList) is not list:
+        contourList = [contourList]
 
-    return sorted_list
+    for currentContour in contourList:
+        if cv2.contourArea(currentContour) > AREA_THRESHOLD:
+            outputList.append(currentContour)
+
+    return outputList
+
+def sortContours(contourList):
+    """
+    this function sorts the list of retro-reflectors from big to small
+    """
+
+    if type(contourList) is not list:
+        contourList = [contourList]
+
+    contourList = sorted(contourList, cmp = lambda y, x: int(cv2.contourArea(x) - cv2.contourArea(y)))
+
+    return contourList
 
 def vision():
-    img = cv2.imread(r'test.png', 1)
+    imgInBGR = cv2.imread(r'test1.jpg')
 
-    if img is not None:
-        hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-        lowerLimit = numpy.array([0, 0, 100])
-        upperLimit = numpy.array([255, 255, 255])
-        imgMask = cv2.inRange(hsv, lowerLimit, upperLimit)
+    # If there was a problem reading the image, exit
+    if imgInBGR is None:
+        raise SystemExit("WTF! cv2.imread returned None!")
 
-        currentContours = cv2.findContours(imgMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
+    lowerLimitInHSV = numpy.array([70, 50, 0], numpy.uint8)
+    upperLimitInHSV = numpy.array([160, 255, 255], numpy.uint8)
+    imgInHSV = cv2.cvtColor(imgInBGR, cv2.COLOR_BGR2HSV)
+    imgMask = cv2.inRange(imgInHSV, lowerLimitInHSV, upperLimitInHSV)
 
-        print "Number of contours = " + str(len(currentContours))
-        #YOLO#SWAG#DOPE
-        # Filtering the current contours by shape
-        contourList = shapeFiltering(currentContours)
+    currentContours = cv2.findContours(imgMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
 
-        # Sort the contours from left to right
-        contourList = sortContours(currentContours)
+    print "Number of contours BEFORE all filtering is " + str(len(currentContours))
+    
+    # Filtering the current contours by area
+    currentContours = areaFiltering(currentContours)
 
-        if len(contourList) == 1:
-            print "Found only one good contour!"
+    print "Number of contours after area filtering is " + str(len(currentContours))
 
-        for currentContour in contourList:
-            print currentContour
+    # Filtering the current contours by shape
+    currentContours = shapeFiltering(currentContours)
 
-            # drawing the contour
-            blankImage = numpy.zeros((480 ,640 ,3), numpy.uint8)
-            cv2.drawContours(blankImage, [currentContour], -1, (255,255,0), 3)
-            cv2.imshow("imaguuuuuu", blankImage)
-            cv2.waitKey()
+    print "Number of contours AFTER all filtering is " + str(len(currentContours))
+
+    # Sort the contours from left to right
+    currentContours = sortContours(currentContours)
+
+    for currentContour in currentContours:
+        # drawing the contour
+        #tempImage = numpy.zeros((480 ,640 ,3), numpy.uint8)
+        tempImage = cv2.imread(r'test1.jpg')
+        cv2.drawContours(tempImage, [currentContour], 0, (255,255,255), 3)
+        cv2.imshow("tyson", tempImage)
+        cv2.waitKey()
 
 
 def main():
