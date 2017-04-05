da4b836bbe33383f1f104b37fe03eca8a37b4b02
Guy Sapire <guysapire@gmail.com>
2017-04-05 18:51:30 +0300

    nevo & tomer are responsible

--- Tyson.py
+++ Tyson.py
@@ -1,230 +1,267 @@
 import time
 import numpy
 import cv2
+from cv2 import *
+import os
 import socket
-import sys
-import traceback
+import math
+import threading
 
 MAX_BOUNDING_RECT_AREA_DIFFERENCE = 1.4
-AREA_THRESHOLD = 500
-HSV_LOW_LIMIT = numpy.array([60, 140, 180], numpy.uint8)
-HSV_HIGH_LIMIT = numpy.array([75, 150, 255], numpy.uint8)
-IMG_WIDTH = 640
-IMG_HEIGHT = 480
-MID_SECTION_LEFT_EDGE = 7*IMG_WIDTH/20
-MID_SECTION_RIGHT_EDGE = 13*IMG_WIDTH/20
-SLEEP_CYCLE_IN_SECONDS = 0.2
+MAXIMUM_HEIGHT_FOR_CONTOUR = 40
+MAXIMUM_AREA_DIFFERENCE = 25
+IMG_WIDTH = 320
+IMG_HEIGHT = 240
+MINIMUM_AREA_THRESHOLD = 100 * (IMG_HEIGHT*IMG_WIDTH / 76800)
+MAXIMUM_AREA_THRESHOLD = 1500 * (IMG_HEIGHT*IMG_WIDTH / 76800)
+SLEEP_CYCLE_IN_SECONDS = 0.00
+CROP_HEIGHT = 73
 roborioSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+while True:
+    try:
+        print "trying to find robo-rio"
+        roborio_ip = socket.gethostbyname('roboRIO-1937-FRC')
+        break
+    except socket.gaierror:
+        roborio_ip = False
+    finally:
+        print "searching in 5 seconds!"
+        time.sleep(5)
+cap = None
+last_v = 215
+bad_streak = 0
+info = {}
+done_count = 0
 
-def HandleSingleContour(singleContour):
-     finalContour = singleContour
 
-    # Calculating the center of the contour
-     currentMoments = cv2.moments(finalContour)
-     centerX = int(currentMoments["m10"] / currentMoments["m00"])
-     ValueToSend = -1 #Indicator
-     if centerX < MID_SECTION_LEFT_EDGE:
-         # Contour is on the left
-         print "Turn left"
-         ValueToSend = 0  
-     elif centerX < MID_SECTION_RIGHT_EDGE:
-         # Contour is in the center
-         print "Move straight"
-         ValueToSend = 1
-     else:
-         # Contour is on the right
-         ValueToSend = 2
+def calibrate(img):
+    old = time.time()
+    global done_count
+    global info
+    info = [None] * 250
 
-     try:
-          roborioSocket.sendto(bytes(str(ValueToSend)), ("roboRIO-1937-FRC", 61937))
-          print("Sent ------------------------------------------------- Sent")  
-     except:
-          print "Error: ",sys.exc_info()[0]
-          traceback.print_exc()
-          raise SystemExit("WTF! Couldn't send to the roborio")
-         
-def shapeFiltering(contourList):
+    def find_usages(high, low):
+        global done_count
+        global info
+        for value in range(low, high):
+            if value % 3 == 0:
+                info[value] = calibration_vision(img, value)
+        done_count += 1
+    cal1 = threading.Thread(target=lambda: find_usages(170, 90))
+    cal1.start()
+    find_usages(250, 170)
+    ratios = []
+    while done_count < 2:
+        pass
+    for i in info:
+        if i is not None:
+            if i[0] == 2 and len(i) == 2:
+                ratios.append((i, i[1] / i[0]))
+    ratios.sort(key=lambda x: x[1])
+    print time.time() - old, " calibration time"
+    try:
+        if len(ratios) > 10:
+            return info.index(ratios[0][0])
+        else:
+            return 256
+    except IndexError:
+        return 256
+
+
+def shape_filtering(contour_list):
     """
     This function receives a list of contours and filters
     out the contours that aren't approximately square
     """
 
-    outputList = []
+    output_list = []
 
-    if type(contourList) is not list:
-        contourList = [contourList]
+    if type(contour_list) is not list:
+        contour_list = [contour_list]
 
-    for currentContour in contourList:
+    for currentContour in contour_list:
         _, _, w, h = cv2.boundingRect(currentContour)
-        boundingRectArea = w * h
-        currentContourArea = cv2.contourArea(currentContour)
-        if boundingRectArea / currentContourArea < MAX_BOUNDING_RECT_AREA_DIFFERENCE:
-            outputList.append(currentContour)
+        bounding_rect_area = w * h
+        current_contour_area = cv2.contourArea(currentContour)
+        if bounding_rect_area / current_contour_area < MAX_BOUNDING_RECT_AREA_DIFFERENCE and w < h:
+            output_list.append(currentContour)
 
-    return outputList
+    return output_list
 
 
-def areaFiltering(contourList):
+def area_filtering(contour_list):
     """
     filters contours that are not above the threshold
     """
 
-    outputList = []
+    output_list = []
 
-    if type(contourList) is not list:
-        contourList = [contourList]
+    if type(contour_list) is not list:
+        contour_list = [contour_list]
 
-    for currentContour in contourList:
-        if cv2.contourArea(currentContour) > AREA_THRESHOLD:
-            outputList.append(currentContour)
+    for current_contour in contour_list:
+        if cv2.contourArea(current_contour) > MINIMUM_AREA_THRESHOLD and cv2.contourArea(current_contour) < MAXIMUM_AREA_THRESHOLD:
+            output_list.append(current_contour)
 
-    return outputList
+    return output_list
 
 
-def sortBySize(contourList):
+def sort_by_size(contour_list):
     """
     this function sorts the list of retro-reflectors from big to small
     """
-
     # If we, for some strange reason, got a single object instead of a list, put that object in a list
-    if type(contourList) is not list:
-        return [contourList]
+    if type(contour_list) is not list:
+        return [contour_list]
+    contour_list = sorted(contour_list, cmp=lambda y, x: int(cv2.contourArea(x) - cv2.contourArea(y)))
+    return contour_list
 
-    compareFunction = lambda y, x: int(cv2.contourArea(x) - cv2.contourArea(y))
-    contourList = sorted(contourList, cmp=compareFunction)
 
-    return contourList
-
-def Statistics(counterLeft,counterStraight,counterRight):
-     if counterLeft+counterStraight+counterRight > 9:
-          if counterLeft > 6:
-               return
-
-def vision():
-    camera = cv2.VideoCapture(0)
-    counterLeft = 0
-    counterStraight = 0
-    counterRight = 0
-
-    # If there was a problem opening the camera, exit
-    if camera.isOpened() is False:
-        raise SystemExit("WTF! cv2.VideoCapture returned None!")
-
+def calibration_vision(img, new_v=215):
+    hsv_low_limit = numpy.array([0, 0, new_v], numpy.uint8)
+    hsv_high_limit = numpy.array([255, 255, 255], numpy.uint8)
+    img_in_bgr = img
+    img_in_bgr = cv2.cvtColor(img_in_bgr, cv2.COLOR_BGR2HSV)
+    img_mask = cv2.inRange(img_in_bgr, hsv_low_limit, hsv_high_limit)
     if cv2.__version__.startswith("3."):
-        camera.set(cv2.CAP_PROP_FRAME_WIDTH, IMG_WIDTH)
-        camera.set(cv2.CAP_PROP_FRAME_HEIGHT, IMG_HEIGHT)
+        current_contours = cv2.findContours(img_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1]
     elif cv2.__version__.startswith("2."):
-        camera.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, IMG_WIDTH)
-        camera.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, IMG_HEIGHT)
+        current_contours = cv2.findContours(img_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
     else:
         raise SystemExit("WTF! What version of openCV are you using?")
+    basic_size = len(current_contours)
+    # Filtering the current contours by area
+    current_contours = area_filtering(current_contours)
 
-    while True:
-         _, imgInBGR = camera.read()
+    # Filtering the current contours by shape
+    current_contours = shape_filtering(current_contours)
 
-         # If there was a problem reading the image, exit
-         if imgInBGR is None:
-             raise SystemExit("WTF! camera.read returned None!")
-
-         imgInHSV = cv2.cvtColor(imgInBGR, cv2.COLOR_BGR2HSV)
-         imgMask = cv2.inRange(imgInHSV, HSV_LOW_LIMIT, HSV_HIGH_LIMIT)
+    # Filters the contours by size (big to small)
+    current_contours = sort_by_size(current_contours)
+    # Checking if the number of Contours is below 2
+    return len(current_contours), basic_size
 
 
-         # Drawing the lines that divide the 3 sections
-         imgInBGR = cv2.line(imgInBGR, (MID_SECTION_LEFT_EDGE, 0), (MID_SECTION_LEFT_EDGE, IMG_HEIGHT), (100, 0, 120), 2)
-         imgInBGR = cv2.line(imgInBGR, (MID_SECTION_RIGHT_EDGE, 0), (MID_SECTION_RIGHT_EDGE, IMG_HEIGHT), (100, 0, 120), 2)
+def vision(img, the_v=215):
+    hsv_low_limit = numpy.array([0, 0, the_v], numpy.uint8)
+    hsv_high_limit = numpy.array([255, 255, 255], numpy.uint8)
+    img_in_bgr = img
+    
+    # If there was a problem reading the image, exit
+    if img_in_bgr is None:
+        raise Exception("WTF! No picture given!")
 
-         if cv2.__version__.startswith("3."):
-             currentContours = cv2.findContours(imgMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1]
-         elif cv2.__version__.startswith("2."):
-             currentContours = cv2.findContours(imgMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
-         else:
-             raise SystemExit("WTF! What version of openCV are you using?")
+    img_in_hsv = cv2.cvtColor(img_in_bgr, cv2.COLOR_BGR2HSV)
+    img_mask = cv2.inRange(img_in_hsv, hsv_low_limit, hsv_high_limit)
 
-         print "Number of contours before all filtering is " + str(len(currentContours))
+    # finding the contours before filtering them
+    if cv2.__version__.startswith("3."):
+        current_contours = cv2.findContours(img_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1]
+    elif cv2.__version__.startswith("2."):
+        current_contours = cv2.findContours(img_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
+    else:
+        raise Exception("WTF! What version of openCV are you using?")
 
-         # Filtering the current contours by area
-         currentContours = areaFiltering(currentContours)
+    # Prints the number of contours before all filtering
+    print "Number of contours before all filtering is " + str(len(current_contours))
 
-         print "Number of contours after area filtering is " + str(len(currentContours))
+    # Filtering the current contours by area
+    current_contours = area_filtering(current_contours)
+    print "Number of contours after area filtering is " + str(len(current_contours))
 
-         # Filtering the current contours by shape
-         currentContours = shapeFiltering(currentContours)
+    # Filtering the current contours by shape
+    current_contours = shape_filtering(current_contours)
+    print "Number of contours after shape filtering is " + str(len(current_contours))
 
-         print "Number of contours after shape filtering is " + str(len(currentContours))
+    # Filters the contours by size (big to small)
+    current_contours = sort_by_size(current_contours)
 
-         currentContours = sortBySize(currentContours)
+    # Checking if the number of Contours is below 2
+    if len(current_contours) < 2:
+        value_to_send = 19370
+        if roborio_ip:
+            roborioSocket.sendto(bytes(str(int(value_to_send))), (roborio_ip, 61937))
+        return False
 
-         # Checking if the number of Contours is below 2 
-         if len(currentContours) < 2:
-             # If found 1 Contour, goes to a function that deals with 1 Contour
-             if len(currentContours) == 1:
-                 HandleSingleContour(currentContours[0])
-                 print "Found one Contour"
-             else:
-                 #If found no Contours, does nothing
-                 print "Couldn't find contours"
-             continue
-         if len(currentContours) > 0:
-              tempImage = numpy.copy(imgInBGR)
-              for contour in currentContours:
-                   cv2.drawContours(tempImage, [contour], 0, (0, 255, 0), 3)
-              cv2.imwrite("picture.png", tempImage)
-              print "WTF! Too many Contours"
-              continue
-        
-         firstContour = currentContours[0]
-         secondContour = currentContours[1]
+    first_contour = current_contours[0]
+    second_contour = current_contours[1]
 
-         # Calculating the center's X of the first contour
-         currentMoments = cv2.moments(firstContour)
-         firstX = int(currentMoments["m10"] / currentMoments["m00"])
+    # Calculating the center's X of the first contour
+    current_moments = cv2.moments(first_contour)
+    first_x = int(current_moments["m10"] / current_moments["m00"])
 
-         # Calculating the center's X of the second contour
-         currentMoments = cv2.moments(secondContour)
-         secondX = int(currentMoments["m10"] / currentMoments["m00"])
+    # Calculating the center's X of the second contour
+    current_moments = cv2.moments(second_contour)
+    second_x = int(current_moments["m10"] / current_moments["m00"])
 
-         # Calculating the center of the contours
-         centerX = (firstX + secondX) / 2
+    # Calculating the center of the contours
+    center_x = (first_x + second_x) / 2
 
-         if centerX <= MID_SECTION_LEFT_EDGE:
-             # Contour is on the left
-             print "Turn left"
-             #currentValue = (centerX / MID_SECTION_LEFT_EDGE) * -1
-             counterLeft += 1
-             currentValue = 0 
-         elif centerX > MID_SECTION_LEFT_EDGE and centerX < MID_SECTION_RIGHT_EDGE:
-             # Contour is in the center
-             print "Move straight"
-             counterStraight += 1
-             currentValue = 1
-         elif centerX >= MID_SECTION_RIGHT_EDGE:
-             # Contour is on the right
-             print "Turn right"
-             counterRight += 1
-             #currentValue = (centerX - MID_SECTION_RIGHT_EDGE)/(IMG_WIDTH - MID_SECTION_RIGHT_EDGE)
-             currentValue = 2
-         try:
-             roborioSocket.sendto(bytes(str(currentValue)), ("roboRIO-1937-FRC", 61937))
-             print("Sent -------------------------------------------------Sent")  
-         except:
-             print "Error: ",sys.exc_info()[0]
-             traceback.print_exc()
-             raise SystemExit("WTF! Couldn't send to the roborio")
+    current_value = (center_x * 6.25)
+    current_value = math.floor(current_value)
+    temp_image = numpy.copy(img_in_bgr)
+    cv2.drawContours(temp_image, [first_contour], 0, (0, 255, 0), 3)
+    cv2.drawContours(temp_image, [second_contour], 0, (0, 255, 0), 3)
+    cv2.imshow("good", temp_image)
+    cv2.waitKey(1)
+    if roborio_ip:
+        roborioSocket.sendto(bytes(str(current_value)), (roborio_ip, 61937))
+        print("----------------We've sent " + str(current_value))
+    return str(current_value)
 
-         # drawing the contour - Currently useless?
-         tempImage = numpy.copy(imgInBGR)
-         cv2.drawContours(tempImage, [finalContour], 0, (0, 255, 0), 3)
-         cv2.imshow("Tyson", tempImage)
-         cv2.waitKey()
-         
-         print "Turn lefts " + str(counterLeft)
-         print "Turn straighta " + str(counterStraight)
-         print "Turn rights " + str(counterRight)
-         time.sleep(SLEEP_CYCLE_IN_SECONDS)
 
-def main():
-    vision()
+def do_frame(img):
+    global last_v
+    global bad_streak
+    if calibration_vision(img, last_v)[0] != 2:
+        if bad_streak >= 3:
+            print "uncalibrated"
+            v = calibrate(img)
+            last_v = v
+        else:
+            bad_streak += 1
+            print "frame unused"
+            return False
+    else:
+        bad_streak = 0
+        v = last_v
+        print "calibrated"
+    if v != 256:
+        print vision(img), "used: ", v
+    else:
+        print "no optimal v"
+        if roborio_ip:
+            roborioSocket.sendto(bytes(str(0)), (roborio_ip, 61937))
+
+
+def main(camera):
+    old = time.time()
+    for i in range(30):
+        got, new_img = camera.read()
+        new_img = cv2.adaptiveThreshold(new_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+        if got:
+            do_frame(new_img)
+            os.system("cls")
+    print "30 frames in ", time.time()-old
+
 
 if __name__ == "__main__":
-    main()
+    robot_cam = cv2.VideoCapture(0)
+    # If there was a problem opening the camera, exit
+    if robot_cam.isOpened() is False:
+        raise Exception("WTF! cv2.VideoCapture returned None!")
+
+    # Checks in which cv version we use and adjusts accordingly
+    if cv2.__version__.startswith("3."):
+        robot_cam.set(cv2.CAP_PROP_FRAME_WIDTH, IMG_WIDTH)
+        robot_cam.set(cv2.CAP_PROP_FRAME_HEIGHT, IMG_HEIGHT)
+    elif cv2.__version__.startswith("2."):
+        robot_cam.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, IMG_WIDTH)
+        robot_cam.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, IMG_HEIGHT)
+    else:
+        raise Exception("WTF! What version of openCV are you using?")
+    while True:
+        main(robot_cam)
+
+""" code fixes """
+# dns lookup every packet replaced with one dns query
