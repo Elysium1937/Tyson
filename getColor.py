aa19b606983a3568684b4d1a9c6a49d51b00aaad
Guy Sapire <guysapire@gmail.com>
2017-04-07 08:18:14 +0300

    Change getColor.py to read a picture from the camera

--- getColor.py
+++ getColor.py
@@ -3,10 +3,30 @@ import numpy
 
 global imgInHSV
 
+IMG_WIDTH = 320
+IMG_HEIGHT = 240
+
 def onMouse(event, x, y, flags, param):
     print imgInHSV[y,x]
 
-imgInBGR = cv2.imread("./test.png", 1)
+camera = cv2.VideoCapture(1)
+
+# If there was a problem opening the camera, exit    
+if camera.isOpened() is False:
+    raise SystemExit("WTF! cv2.VideoCapture returned None!")
+
+# Checks in which cv version we use and adjusts accordingly
+if cv2.__version__.startswith("3."):
+    camera.set(cv2.CAP_PROP_FRAME_WIDTH, IMG_WIDTH)
+    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, IMG_HEIGHT)
+elif cv2.__version__.startswith("2."):
+    camera.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, IMG_WIDTH)
+    camera.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, IMG_HEIGHT)
+else:
+    raise SystemExit("WTF! What version of openCV are you using?")
+
+_, imgInBGR = camera.read()
+
 imgInHSV = cv2.cvtColor(imgInBGR, cv2.COLOR_BGR2HSV)
 cv2.namedWindow("image")
 cv2.setMouseCallback("image", onMouse)
